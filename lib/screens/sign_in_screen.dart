728515ccdf1f73bcdb3eb82914a01a811654bc90
Umar Hasan <umrhsnE2020@gmail.com>
2022-06-29 01:43:11 +0200

    separating stack components to custom_stack.dart

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -3,6 +3,8 @@ import 'dart:math';
 import 'package:fashion_daily/utils/constants.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/custom_stack.dart';
+
 class SignInScreen extends StatefulWidget {
   const SignInScreen({Key? key}) : super(key: key);
 
@@ -19,65 +21,8 @@ class _SignInScreenState extends State<SignInScreen> {
           // TODO:
           //   - make stack reusable
           //   - make sizes and translation values flexible via MediaQuery
-          ClipRRect(
-            child: Stack(
-              children: [
-                Container(
-                  height: MediaQuery.of(context).size.height / 3,
-                  width: double.infinity,
-                  color: Constants.kBlue,
-                ),
-                Transform.rotate(
-                  angle: pi / 14,
-                  child: Container(
-                    height: 170,
-                    width: 170,
-                    color: Constants.kPink,
-                    transform: Matrix4.translationValues(-80.0, -10.0, 0.0),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Container(
-                    transform: Matrix4.translationValues(20.0, -65.0, 0.0),
-                    child: Transform.rotate(
-                      angle: -pi / 6,
-                      child: const Image(
-                        height: 180,
-                        width: 180,
-                        color: Constants.kLightBlue,
-                        image: AssetImage('assets/images/dotted_square.png'),
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: Transform.rotate(
-                    alignment: Alignment.bottomRight,
-                    angle: pi / 8,
-                    child: Container(
-                      height: 165,
-                      width: 165,
-                      color: Constants.kYellow,
-                      transform: Matrix4.translationValues(120.0, 155.0, 0.0),
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomLeft,
-                  child: Transform(
-                    transform: Matrix4.translationValues(120, 165, 0),
-                    child: ClipOval(
-                      child: Container(
-                          height: 90, width: 90, color: Constants.kLightBlue),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
+          //   currently it's just moved to another file to lessen the widget tree size
+          const CustomStack(),
           const Align(
             alignment: Alignment.centerLeft,
             child: Text('Welcome to Fashion Daily',
